d16f2facca6d50d9ea3dfb66ea9bc26de892af33
TheDiscoScript <wollmada@gmail.com>
2023-12-03 10:05:56 +0100

    feat: removed .js files from repo

--- 2023/utilities/get-rows.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRows = void 0;
-function getRows(input) {
-    return input.split('\n');
-}
-exports.getRows = getRows;
