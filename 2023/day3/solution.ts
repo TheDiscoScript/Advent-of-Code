cf4eb02f79e58f505342e2350b470da8b6c57879
TheDiscoScript <wollmada@gmail.com>
2023-12-03 14:06:57 +0100

    feat: added part 2

--- 2023/day3/solution.ts
+++ 2023/day3/solution.ts
@@ -156,3 +156,72 @@ const runInputData = () => {
 };
 console.log('Test Input: ' + returnSumOfNumbersAdjectendToSymbol(testString));
 console.log('Real Input: ' + runInputData());
+
+//PART TWO//
+const returnSumOfMultipliedPairNumbersConnectedBySymbol = (inputString: string) => {
+	const rows = getRows(inputString);
+	const numbersToSum: number[] = [];
+	const database: RowWithNumberAndSymbolPositions = {};
+	for (let i = 0; i < rows.length; i++) {
+		const row = rows[i];
+		const [numbers, symbols] = getNumbersAndSymbolsPositions(row);
+		database[i] = [numbers, symbols];
+	}
+	for (let y = 0; y < rows.length; y++) {
+		const previousRow = database[y - 1];
+		const currentRow = database[y];
+		const nextRow = database[y + 1];
+
+		//similar stuff, using symbols instead of numbers
+		if (currentRow[1].length === 0) continue; //no symbols in row
+		for (let symbolL = 0; symbolL < currentRow[1].length; symbolL++) {
+			const currentSymbolAdjacentNumbers = new Set<number>();
+			//this might be not needed - I think there arent any symbols, that are next to each other
+			const currentSymbolPositions = currentRow[1][symbolL][Object.keys(currentRow[1][symbolL])[0]];
+
+			// now we need to check for any numbeer in vicinity of point - there can be max 2 numbers
+			// 1. check previous row
+			if (previousRow !== undefined && previousRow[0].length !== 0) {
+				for (let numL = 0; numL < previousRow[0].length; numL++) {
+					const currentNumPositions = previousRow[0][numL][Object.keys(previousRow[0][numL])[0] as unknown as number];
+					if (helperFunctionSymbolVicinity(currentSymbolPositions, currentNumPositions)) {
+						currentSymbolAdjacentNumbers.add(Number(Object.keys(previousRow[0][numL])[0]));
+					}
+				}
+			}
+			// 2. check current row
+			if (currentRow[0].length !== 0) {
+				for (let numL = 0; numL < currentRow[0].length; numL++) {
+					const currentNumPositions = currentRow[0][numL][Object.keys(currentRow[0][numL])[0] as unknown as number];
+					if (helperFunctionSymbolVicinity(currentSymbolPositions, currentNumPositions)) {
+						currentSymbolAdjacentNumbers.add(Number(Object.keys(currentRow[0][numL])[0]));
+					}
+				}
+			}
+			// 3. check next row
+			if (nextRow !== undefined && nextRow[0].length !== 0) {
+				for (let numL = 0; numL < nextRow[0].length; numL++) {
+					const currentNumPositions = nextRow[0][numL][Object.keys(nextRow[0][numL])[0] as unknown as number];
+					if (helperFunctionSymbolVicinity(currentSymbolPositions, currentNumPositions)) {
+						currentSymbolAdjacentNumbers.add(Number(Object.keys(nextRow[0][numL])[0]));
+					}
+				}
+			}
+
+			if (currentSymbolAdjacentNumbers.size === 2) {
+				const currentSymbolAdjacentNumbersArray = Array.from(currentSymbolAdjacentNumbers);
+				const multipliedPair = currentSymbolAdjacentNumbersArray[0] * currentSymbolAdjacentNumbersArray[1];
+				numbersToSum.push(multipliedPair);
+			}
+		}
+	}
+	return getSumOfValues(numbersToSum);
+};
+const runInputDataPart2 = () => {
+	const inputData = readInputFile('day3', 'input.txt');
+	if (!inputData) throw new Error('No input data');
+	return returnSumOfMultipliedPairNumbersConnectedBySymbol(inputData);
+};
+
+console.log('Test Input Part 2: ' + returnSumOfMultipliedPairNumbersConnectedBySymbol(testString));
+console.log('Real Input Part 2: ' + runInputDataPart2());
